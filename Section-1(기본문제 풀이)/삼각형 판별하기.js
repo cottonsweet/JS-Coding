721529dfe24cad7e96a536ed65a9873629baf504
cottonsweet <whitecandy777@naver.com>
2024-05-27 13:47:55 +0900

    섹션1- 삼각형 판별하기 (코드수정)

--- Section-1(기본문제 풀이)/삼각형 판별하기.js
+++ Section-1(기본문제 풀이)/삼각형 판별하기.js
@@ -23,13 +23,24 @@
  */
 
 function solution(a, b, c) {
-  let answer = "YES",
-    max;
+  let answer = "YES";
+  let max;
   let sum = a + b + c;
-  if (a > b) max = a;
-  else max = b;
-  if (c > max) max = c;
-  if (sum - max <= max) answer = "NO";
+
+  if (a > b) {
+    max = a;
+  } else {
+    max = b;
+  }
+
+  if (c > max) {
+    max = c;
+  }
+
+  if (sum - max <= max) {
+    answer = "NO";
+  }
+
   return answer;
 }
 
